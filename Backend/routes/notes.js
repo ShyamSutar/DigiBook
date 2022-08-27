265ba6a531882275795f045b4b7869d93deace52
shyamsutar <shyamsutar629@gmail.com>
2022-08-27 21:27:57 +0530

    api/note/updatenote

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -55,4 +55,42 @@ router.post(
   }
 );
 
+//Route 3: Update an existing note using: PUT
+
+router.put(
+    "/updatenote/:id",
+    fetchUser,
+    async (req, res) => {
+
+        const{title, description, tag} = req.body;
+
+        //Create a newNote object
+        const newNote = {};
+        if(title){
+            {newNote.title = title};
+        }
+        if(description){
+            {newNote.description = description};
+        }
+        if(tag){
+            {newNote.tag = tag};
+        }
+
+
+        //Find the note to be updated and update it
+
+        let note = await Notes.findById(req.params.id);
+
+        if(!note){
+            res.status(404).send("Not found");
+        }
+
+        if(note.user.toString() != req.user.id){
+            return res.status(401).send("Not allowed");
+        }
+
+        note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
+
+        res.json(note);
+    })
 module.exports = router;
